60a2668a2ec3752ccd760c1c8d51588a2352a67b
776A0A <240508428@qq.com>
2021-01-25 18:23:56 +0800

    build(/): 继续配置 eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,17 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		node: true
-	},
-	'extends': [
-		'eslint:recommended',
-	],
-	'parserOptions': {
-		'ecmaVersion': 12,
-		'sourceType': 'module'
-	},
-	'rules': {
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
-	}
-}
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  parserOptions: {
+    sourceType: 'module'
+  },
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    'prettier/prettier': 'error'
+  },
+  parser: 'babel-eslint'
+};
--- src/index.js
+++ src/index.js
@@ -2,48 +2,48 @@
 export const invokeWithErrorCatch = ({
   fn = noop,
   errorHandler = noop,
-  log = true,
+  log = true
 }) => {
   try {
-    return fn()
+    return fn();
   } catch (err) {
-    log && logError(err)
+    log && logError(err);
     return (
       errorHandler && typeof errorHandler === 'function' && errorHandler(err)
-    )
+    );
   }
-}
+};
 
 // 返回包裹过错误处理的函数
-export const wrapError = (fn) => {
+export const wrapError = fn => {
   return function wrappedFn(...args) {
-    return invokeWithErrorCatch({ fn: () => fn.call(this, ...args) })
-  }
-}
+    return invokeWithErrorCatch({ fn: () => fn.call(this, ...args) });
+  };
+};
 
 // 用于捕获promise中出现的错误
-export const wrapPromiseWithCache = (fn) => {
+export const wrapPromiseWithCache = fn => {
   return function wrappedPromise(...args) {
     return invokeWithErrorCatch({
-      fn: () => fn.call(this, ...args).catch((err) => logError(err)),
-    })
-  }
-}
+      fn: () => fn.call(this, ...args).catch(err => logError(err))
+    });
+  };
+};
 
-export const logError = (err) => {
+export const logError = err => {
   if (process.env.NODE_ENV === 'development') {
-    console.error(`(DEV ONLY)\n${err}`)
+    console.error(`(DEV ONLY)\n${err}`);
   } else {
-    console.warn(`(logError)\n${err}`)
+    console.warn(`(logError)\n${err}`);
   }
-}
+};
 
-export const stringify = (val) =>
-  invokeWithErrorCatch({ fn: () => JSON.stringify(val) })
-export const unstringify = (val) =>
-  invokeWithErrorCatch({ fn: () => JSON.parse(val) })
+export const stringify = val =>
+  invokeWithErrorCatch({ fn: () => JSON.stringify(val) });
+export const unstringify = val =>
+  invokeWithErrorCatch({ fn: () => JSON.parse(val) });
 
-export const noop = () => {}
+export const noop = () => {};
 
 export default {
   invokeWithErrorCatch,
@@ -52,5 +52,5 @@ export default {
   logError,
   stringify,
   unstringify,
-  noop,
-}
+  noop
+};
